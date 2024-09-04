4e9448bf7fd935296c8ce0d932e2ad4bbeffdc33
muhammadidzhar15 <idzharpratama15@gmail.com>
2024-09-05 00:19:30 +0700

    "first commit"

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      fontFamily: {
+        primary: ["Poppins", "sans-serif"],
+      },
+      backgroundColor: {
+        primary: "#0D1219",
+      },
+    },
+    colors: {
+      primary: "#3498db",
+    },
+  },
+};
